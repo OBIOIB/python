021bbeeaaf727fc5745b573abd422d8aa36e3a0c
io <kapi.name.pooh@gmail.com>
2023-08-07 09:36:05 +0900

    第2章まで

---
+++ c2_1.py
@@ -0,0 +1,9 @@
+network = 88
+database = 95
+security = 90
+total = network+database+security
+avg = total/3
+print('合計点:{}'.format(total))
+#print(f'合計点:{total}')
+print('平均点:{}'.format(avg))
+#print(f'平均点:{avg}')
---
+++ c2_3.py
@@ -0,0 +1,12 @@
+scores = {'network':60,'database':80,'security':60}
+members=['松田','浅木','工藤']
+tp1 = tuple(members)#リストmembersを素材にしてタプルを生成
+print(tp1)
+ls1 = list(scores)#scoresのキーを集めたリストを生成
+print(ls1)
+st1=set(scores.value()))#scoresのvalueを材料としてsetを生成
+n1 = int('8')
+codes=['#f00','#0f0','#00f']
+colors = ['red','green','blue']
+dict1 = dict(zip(codes,colors))
+print(dict1)
